69d77f568755816b4328930ef30353c7c3ab5a55
Stephen Algeo <algeos@yw.co.uk>
2020-02-25 19:00:43 +0000

    Add basic info and phone link

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>J Peel Electrical</h1>
+        <h3>Domestic Electrician</h3>
       </header>
+      <section>
+        <a target="_blank" href="tel:+447929850056" rel="noopener noreferrer">
+          <span id="primary_cta_1">
+            <span>Call </span>
+            <span dir="ltr">07929 850056</span>
+          </span>
+        </a>
+      </section>
     </div>
   );
 }
