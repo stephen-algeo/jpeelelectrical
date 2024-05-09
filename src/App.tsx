6768b4aaedd593893038658922c6f401c36d59dc
Stephen Algeo <salgeo@williamhill.us>
2024-05-09 11:59:58 +0100

    2024 updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          alt="jpeel electrical logo"
+          className="main-logo"
+          src="./jpeel-logo.png"
+        />
+        <h2>Local domestic electrician</h2>
+        <img className="hero" src="images/jase.jpeg"></img>
       </header>
+      <section>
+        <div></div>
+
+        <div className="info">
+          <p>Over 20 years of experience working in people's homes</p>
+          <p>Fully Qualified and registered.</p>
+          <p>
+            No callout charge, happy to come look
+            <br /> and advise for free.
+          </p>
+          <p>
+            All work undertaken, from putting up a light fitting
+            <br /> to full house rewires.
+          </p>
+
+          <p>
+            Areas covered:
+            <ul>
+              <li>Cleckheaton</li>
+              <li>Huddersfield</li>
+              <li>Wakefield</li>
+              <li>Halifax</li>
+              <li>Bradford</li>
+              <li>Brighouse</li>
+              <li>Morley</li>
+              <li>and all surrounding areas</li>
+            </ul>
+          </p>
+        </div>
+        <div className="images">
+          <img src="images/extension-downlights.jpeg" />
+          <img src="images/outside-lights.jpeg" />
+          <img src="images/fusebox.jpeg" />
+          <img src="images/hanging-light.jpeg" />
+        </div>
+        <div className="call">
+          <a
+            className="btn"
+            target="_blank"
+            href="tel:+447929850056"
+            rel="noopener noreferrer"
+          >
+            <span id="primary_cta_1">
+              <span>Call or text Jason </span>
+              <br />
+              <span>07929 850 056</span>
+            </span>
+          </a>
+        </div>
+        <div>
+          <p className="email">
+            {"email: "}
+            <a href="mailto:jpeelelectrical@gmail.com">
+              jpeelelectrical@gmail.com
+            </a>
+          </p>
+        </div>
+        <div className="images">
+          <img src="images/qualification-nic-eic-domestic-install.jpeg" />
+        </div>
+      </section>
     </div>
   );
 }
